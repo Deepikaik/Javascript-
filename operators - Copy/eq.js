ae1a07967ae76d91b6f35a60eb578d662b312e75
Deepikaik <deepikabhagavati2001@gmail.com>
2024-07-24 14:05:17 +0530

    'js'

---
+++ operators - Copy/eq.js
@@ -0,0 +1,11 @@
+//strict equality -checks datatype
+//equality -checks only content
+
+let a=10;
+let b=20;
+let c="10"
+let d=188
+ 
+console.log(a==c) //true
+console.log(a===c)//false
+console.log(d>=100 && d<=999)
