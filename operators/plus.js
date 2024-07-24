057fd91f74ed6e247d47c3872c30336c636fe91e
Deepikaik <deepikabhagavati2001@gmail.com>
2024-07-24 14:03:49 +0530

    'js'

---
+++ operators/plus.js
@@ -0,0 +1,17 @@
+var a=10;
+var b=20.5;
+var c="30";
+var d="Rahul";
+var e="Y";
+var f=true;
+var g=false;
+var h;
+// plus operator
+
+console.log(a+b)
+console.log(a+c)
+console.log(a+e)
+console.log(a+f)
+console.log(a+g)
+console.log(a+h)
+
