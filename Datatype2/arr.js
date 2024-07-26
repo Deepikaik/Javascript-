985582c389cd271b72c48dc9c012e5e4e6001c1d
Deepikaik <deepikabhagavati2001@gmail.com>
2024-07-26 13:18:18 +0530

    'js'

---
+++ Datatype2/arr.js
@@ -0,0 +1,26 @@
+let eid=[101,102,103,104];
+console.log(eid)
+
+console.log(eid[0])
+console.log(eid[1])
+console.log(eid[2])
+console.log(eid[3])
+console.log(eid[4])
+
+//executing using for
+for(var i=0;i<=3;i++){
+    console.log(eid[i])
+}
+
+//executing using for of
+let enames=["rahul","sonia","priya","modi"]
+/*for(var i=0;i<=3;i++){
+    console.log(enames[i])}
+    /*
+
+    /*var i=0
+
+
+    for (ename of enames){
+        console.log(ename)
+    }
